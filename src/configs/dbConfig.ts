b38724e5a1384f0af39ff43ac2171a3cd0e22868
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-04-02 02:10:16 +0530

    refactor - changes in ConstantEnv

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -1,6 +1,7 @@
-import ConstantEnv, { DATABASE_NAME } from "@constants/index";
+import { DATABASE_NAME } from "@constants/index";
 
 export const get_mongo_connection = () => {
   const db_name = DATABASE_NAME || 'mentorsklub';
-  return `${ConstantEnv.DATABASE_URI}/${db_name}`;
+  console.log(`${process.env.DATABASE_URI}/${db_name}`)
+  return `${process.env.DATABASE_URI}/${db_name}`;
 };
--- src/modules/mentor/auth/services/auth.service.ts
+++ src/modules/mentor/auth/services/auth.service.ts
@@ -1,11 +1,84 @@
-import { Injectable } from "@nestjs/common";
-
+import { DATABASE_NAME } from '@constants/index';
+import { User } from '@modules/common/users/models/users.model';
+import { Mentor } from '@modules/mentor/mentor_user/models/mentor.model';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-    constructor(
+  // constructor(
+  //   private cypherSvc: CypherService,
+  //   private jwtSvc: JwtService,
+  //   private configSvc: ConfigService,
+  //   @InjectModel(User.name, DATABASE_NAME) private userModel: Model<User>,
+  //   @InjectModel(Mentor.name, DATABASE_NAME) private mentorModel: Model<Mentor>,
+  // ) {}
 
-    ) {}
+  // #createJwtPayload(user: User) {
+  //   const { email } = user;
+  //   const accessToken = this.jwtSvc.sign(
+  //     { email },
+  //     {
+  //       secret: this.configSvc.get<string>('JWT_SECRET'),
+  //       expiresIn: `${this.configSvc.get<number>('JWT_EXPIRY_TIME')}d`,
+  //     },
+  //   );
+  //   this.jwtSvc.decode;
+  //   return accessToken;
+  // }
+  // #createRefreshJwtPayload(user: User) {
+  //   const { email } = user;
 
-    
+  //   const accessToken = this.jwtSvc.sign(
+  //     { email },
+  //     {
+  //       secret: this.configSvc.get<string>('JWT_SECRET'),
+  //       expiresIn: `${this.configSvc.get<number>('JWT_EXPIRY_TIME')}d`,
+  //     },
+  //   );
+  //   this.jwtSvc.decode;
+  //   return accessToken;
+  // }
+
+  // async login() {}
+
+  // async signUp(
+  //   createAuthDto: CreateAuthDto,
+  // ): ApiResponse<AuthLoginSuccessResponse> {
+  //   try {
+  //     const { email, password, username, firstName, lastName, role } =
+  //       createAuthDto;
+  //     const isUser = await this.userModel
+  //       .findOne({
+  //         email,
+  //       })
+  //       .exec();
+
+  //     if (isUser) {
+  //       throw new BadRequestException('User already Exists');
+  //     }
+  //     const hashedPassword = await bcrypt.hash(password, 8);
+
+  //     const newUser = await this.userModel.create({
+  //       email,
+  //       password: hashedPassword,
+  //       username,
+  //       firstName,
+  //       lastName,
+  //       role,
+  //     });
+
+  //     const jwt = await this.#createJwtPayload(newUser);
+  //     const refershJwt = await this.#createRefreshJwtPayload(newUser);
+
+  //     return {
+  //       authToken: jwt,
+  //       id: newUser._id,
+  //     };
+  //   } catch (err) {
+  //     return err.message;
+  //   }
+  // }
 }
